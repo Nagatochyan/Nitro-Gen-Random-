c97abbb0956164edd2193b063e73b0a3b60d1939
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-04-01 08:56:48 +0900

    Add files via upload

---
+++ nitro.py
@@ -0,0 +1,12 @@
+
+import os
+from time import sleep
+import requests
+import random
+import string
+from colorama import Fore
+num = int(input("何回送りますか？"))
+for i in range(num):
+    a="https://discord.gift/"
+    code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(24)])
+    print(a +code)
