486fa4019b15cc071fb32bdeedb88baaa036ba63
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-04-01 09:00:18 +0900

    Update nitro.py

--- nitro.py
+++ nitro.py
@@ -8,5 +8,5 @@ from colorama import Fore
 num = int(input("何回送りますか？"))
 for i in range(num):
     a="https://discord.gift/"
-    code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(24)])
+    code = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(16)])
     print(a +code)
